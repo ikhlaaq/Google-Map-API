094d92d771c48d8044f674ca68e8d887481c1abe
ikhlaaq <ikhlaak@hotmail.com>
2018-04-13 14:55:48 +0200

    API

---
+++ app.js
@@ -0,0 +1,14 @@
+
+//Google Map API
+
+function initMap() {
+    var uluru = { lat: 56.0381844, lng: 12.6963136 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
